7cf79b8b196e583ce096a6aa17886fc86e5c885d
Yurii27 <65013961+Yurii27@users.noreply.github.com>
2020-05-08 11:24:42 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,5 +3,9 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday B18");
+    
+    
+    
+            system.out.println("Happy birthday B18 again, this line is edit from GitHub");
     }
 }
