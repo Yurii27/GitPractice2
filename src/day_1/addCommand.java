cb131246c8a0a0fc482512592228b82c67f7f999
Irini Trydub <desara.gjoni@hotmail.com>
2020-05-08 10:52:46 -0400

    add addCommand.java

---
+++ src/day_1/addCommand.java
@@ -0,0 +1,14 @@
+package day_1;
+
+public class addCommand {
+    public static void main(String[] args) {
+
+
+
+
+        //will see you in Github
+
+
+        //I made chaned here when I created addCommand class
+    }
+}
